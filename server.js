92ac985ceca5917fc1e064181a83b523febbfc62
rxxzeee <salatenko7@gmail.com>
2025-03-26 22:05:23 +0200

    bobr

--- server.js
+++ server.js
@@ -8,23 +8,57 @@ const port = 3000;
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
-  database: 'sevice_db',
+  database: 'service_db',
   password: '1234',
   port: 5432,
 });
 
-// Маршрут для перевірки підключення до бази даних
-app.get('/db', async (req, res) => {
-  try {
-    const client = await pool.connect();
-    const result = await client.query('SELECT NOW()');
-    res.send(result.rows);
-    client.release();
-  } catch (err) {
-    console.error(err);
-    res.send('Error ' + err);
-  }
+app.use(express.json());
+
+//Додавання нового юзера (Create)
+app.post('/users', async(req,res) =>{
+    const {id, username, password} = req.body;
+    try {
+        const result = await pool.query(
+            'INSERT INTO users (id, username, password) VALUES ($1, $2, $3) RETURNING *',
+            [id,username,password]
+        );
+        res.status(201).json(result.rows[0]);
+    } catch(err) {
+        console.error(err);
+        res.status(500).json({error: 'Internal Server Error'});
+    }
 });
+
+//Отримання всіх юзерів (Read)
+app.get('users', async (req,res) =>{
+    try{
+        const result = await pool.query('SELECT * FROM users');
+        res.json(results.row[0]);
+    }catch (err){
+        console.error(err);
+        res.status(500).json({error: 'Internal Server Error'});
+    }
+});
+
+// Оновлення інформації про книгу (Update)
+app.put('/users/:id', async(req,res) => {
+    const { id } = req.params;
+    const { username, password } = req.body;
+    try{
+        const result = await pool.query(
+            'UPDATE users SET username = $1, password = $2, WHERE id = $4 RETURNING *',
+            [id, username,password]
+        );
+        if (result.rows.length === 0) {
+            return res.status(404).json({ error: 'Book not found' });
+          }
+          res.json(result.rows[0]);
+        } catch (err) {
+          console.error(err);
+          res.status(500).json({ error: 'Internal Server Error' });
+        }
+      });
 
 // Запуск сервера
 app.listen(port, () => {
